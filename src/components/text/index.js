fe74aca3e56559c83fe4db48a0420804ce372e49
renevanderark <r.van.der.ark@gmail.com>
2016-02-05 15:21:20 +0100

    highlight visible text and style witness menu

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 
-
 class Text extends React.Component {
+
+	isVisibleNode(idx, activeIdx, currentReading, dist = 3) {
+		if(!activeIdx.length) { return false; }
+		if(activeIdx < dist) { return idx <= dist * 2; }
+		if(activeIdx > currentReading.length - dist - 1) { return idx >= currentReading.length - 1 - dist * 2; }
+		if(Math.abs(activeIdx - idx) <= dist) { return true; }
+		return false;
+	}
+
 	render() {
+		let currentReading = this.props.text.readings.filter((r) => r.witnesses.indexOf(this.props.text.currentWitness) > -1);
+		let activeNodeIdx = currentReading.map((r, i) => r.id === this.props.graph.activeNode ? i : -1).filter((idx) => idx > -1);
+
 		return (
 			<div className="text">
 				<ul>
@@ -15,8 +26,13 @@ class Text extends React.Component {
 					))}
 				</ul>
 				<div className="reading">
-					{this.props.text.readings.filter((r) => r.witnesses.indexOf(this.props.text.currentWitness) > -1).map((r) => (
-						<span key={r.id} onClick={() => this.props.onSetActiveNode(r.id) }>{r.text}</span>
+					{currentReading.map((r, idx) => (
+						<span
+							className={this.isVisibleNode(idx, activeNodeIdx, currentReading) ? "visible" : ""}
+							key={r.id} 
+							onClick={() => this.props.onSetActiveNode(r.id) }>
+							{r.text}
+						</span>
 					))}
 				</div>
 			</div>
@@ -25,6 +41,7 @@ class Text extends React.Component {
 }
 
 Text.propTypes = {
+	graph: React.PropTypes.object,
 	onSelectWitness: React.PropTypes.func,
 	onSetActiveNode: React.PropTypes.func,
 	text: React.PropTypes.object
