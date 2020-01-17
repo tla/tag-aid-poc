aac7a5244a713f92ba3ae9d33575ae6b97f9b79e
Athena Capsis <acapsis@accademiaDegliIncogniti.net>
2020-01-17 12:31:28 -0800

    text pane works with updated html

--- src/components/Edition/TextPane.js
+++ src/components/Edition/TextPane.js
@@ -7,7 +7,8 @@ import Typography from '@material-ui/core/Typography'
 
 const TextPane =(props) => {
 
-      const {sectionId, reading, onSelectNode, selectedNode, selectedSentence, onSelectSentence} = props;
+      const {sectionId, reading, onSelectNode, selectedNode, selectedSentence, onSelectSentence,
+      persons} = props;
       const [rawText, setRawText] = useState();
       const [enTitle, setEnTitle] = useState();
       const [arTitle, setArTitle] = useState();
@@ -16,29 +17,29 @@ const TextPane =(props) => {
       const parserOptions = {
             replace: function({attribs,children}) {
                   if( attribs && attribs.id ){
-                              let attribNodeId = attribs.id.substring(5);
+                              let nodeId = reading ==="Translation" ? attribs.id.split('-')[1] : attribs.id;
+                              let rank = reading ==="Translation" ? attribs.id.split('-')[0] : attribs.key;
 // this can be further refactored - it was in transition... 
                               if( reading === "Translation"){
-                                    let selected= props.selectedSentence ? props.selectedSentence.start === attribNodeId  : false;
+                                    let selected= props.selectedSentence ? props.selectedSentence.startId === nodeId  : false;
                                     if(selected) {
                                                 return <span style={{backgroundColor:'yellow'}} >
                                                       {domToReact(children,parserOptions)}</span>
                                     } else {
-                                                return <span onClick={()=>{console.log("selected"); handleHighlight(attribNodeId, attribs.key)}}
+                                                return <span onClick={()=>{console.log("selected"); handleHighlight(attribs.id, attribs.key)}}
                                                 >{domToReact(children,parserOptions)}</span>
                                     }
                               } else {
 
-                                    let selected= props.selectedNode ? props.selectedNode === attribNodeId  : false;
-                                    let inSelectedSentence = props.selectedSentence? (parseInt(attribNodeId) >= parseInt(selectedSentence.start) && parseInt(attribNodeId)<= parseInt(selectedSentence.end) ) : false;
+                                    let selected= props.selectedNode ? props.selectedNode === nodeId : false;
+                                    let inSelectedSentence = props.selectedSentence? (rank >= selectedSentence.startRank && rank <= selectedSentence.endRank ) : false;
                                     let textStyle={
                                           color: selected? 'red':'black',
                                           backgroundColor: inSelectedSentence? 'yellow':'transparent'
                                     }
                                    
-                                          return <span style={textStyle} onClick={()=>{console.log("selected"); handleSelected(attribs.id)}} >
+                                          return <span style={textStyle} onClick={()=>{console.log("selected"); handleSelected(nodeId)}} >
                                                 {domToReact(children,parserOptions)}</span>
-                                 
 
                               }
 
@@ -68,24 +69,14 @@ const TextPane =(props) => {
            });
       },[props.sectionId, props.reading])
 
-      
 
       useEffect(()=>{
           if(! rawText )
           return;
             let parsed =  Parser(rawText, parserOptions);
             setTextHTML(parsed);
-      },[ props.selectedNode])
+      },[ selectedNode, selectedSentence, persons])
 
-
-      useEffect(()=>{
-            if(! rawText )
-            return;
-              let parsed =  Parser(rawText, parserOptions);
-              setTextHTML(parsed);
-        },[ props.selectedSentence])
-      
-       
       return (
            <div>
                         <Typography variant="h5" style={{textAlign:'center', marginBottom:'6px'}}>
@@ -105,13 +96,13 @@ const TextPane =(props) => {
            </div>
           )
 
+          // contains rank and id
       function handleHighlight( startNodeId, endNodeId){
             onSelectSentence(startNodeId, endNodeId)
       }
 
       function handleSelected( textNodeId){
-            let trimmedId = textNodeId.substring(5)
-            onSelectNode(trimmedId)
+            onSelectNode(textNodeId)
       }
 
       
