2e5f2d0fdc3525a3f059f19d328237e9cab27405
Athena Capsis <acapsis@accademiaDegliIncogniti.net>
2020-04-02 18:01:58 -0700

    generate dates script

--- script/generateDates.js
+++ script/generateDates.js
@@ -19,8 +19,13 @@ async function generateDates(){
 
       await fetchDateInfo();
 
+      await fetchTranslations();
+
       writeDateList();
-      console.log('end read and generate date info', moment().format('mm ss'))
+
+      console.log('end read and generate date info', moment().format('mm ss'));
+
+
 // a possible api call to look up the reading, you can pass a begin and end node
 // we will start with getting the translation though - 
       //http://www.example.com/tradition/tradId/section/sectionId/lemmatext/
@@ -28,7 +33,7 @@ async function generateDates(){
    
       function writeDateList(){
             makeDirectory();
-            let filePath=`${outdir}/"ChronicleDate.json"`
+            let filePath=`${outdir}/chronicleDates.json`
             writeFile(filePath, JSON.stringify(dateList))
       }
 
@@ -37,53 +42,138 @@ async function generateDates(){
 
             try{
                   let allReadings  = await axios.get(`${baseURL}/readings`,{auth});
-                 
-          
+                  console.log("all readings = individual word nodes fetched")
+                  allReadings.data.forEach( reading=>{
+                        if( reading.is_lemma)
+                              wordHash[reading.id] = {
+                                    section:reading.section
+                              }
+                  })
 
-            console.log("all readings = individual word nodes fetched")
-            allReadings.data.forEach( reading=>{
-                  if( reading.is_lemma)
-                        wordHash[reading.id] = {
-                              section:reading.section
-                        }
-            })
-
-      }catch( error){
-            console.log(`error fetching all readings ${error} `);
-           
-      }
+            }catch( error){
+                  console.log(`error fetching all readings ${error} `);
+            
+            }
       }
 
 
       async function fetchDateInfo(){
-            let dates = fetchDateAnnotations();
-            let daterefs = fetchDateRefs();
+            let dates = new Promise( resolve =>{
+                  fetchDateAnnotations()
+                  .then(dates=>{
+                        resolve(dates)
+                  } )
+            });
+
+            let daterefs = new Promise( resolve =>{
+                  fetchDateRefs()
+                  .then(refs=>{
+                        resolve(refs)
+                  })
+            });
+
+
             let dateInfo = await Promise.all([dates, daterefs]);
+          
             console.log('dates and daterefs fetched')
-            dates.forEach( date => {
+            dateInfo[0].forEach( date => {
                   let refId = date.links[0].target;
-                  let dateRef = daterefs.find( r=>{ return r.id === refId});
+                  let dateRef = dateInfo[1].find( r=>{ return parseInt(r.id ) === refId});
+                  if ( ! dateRef ){
+                        console.log( 'no dateref for date id ', date.id);
+                        return;
+                  }
                   let beginNode = dateRef.links[0].type==="BEGIN"? dateRef.links[0].target:dateRef.links[1].target;
                   let endNode = dateRef.links[1].type==="END"? dateRef.links[1].target:dateRef.links[0].target;
-                  let section = wordHash[beginNode];
+                  let section = wordHash[beginNode].section;
+                  let earliestDate, latestDate;
+                  if( date.properties.notBefore ) 
+                        earliestDate = moment().year(date.properties.notBefore.year).dayOfYear(date.properties.notBefore.dayOfYear);
+                   if( date.properties.notAfter)
+                        latestDate = moment().year(date.properties.notAfter.year).dayOfYear(date.properties.notAfter.dayOfYear)
                   dateList.push({
-                        earliestDate: moment().year(date.properties.notBefore.year).dayOfYear(date.properties.notBefore.dayOfYear),
-                        latestDate: moment().year(date.properties.notAfter.year).dayOfYear(date.properties.notAfter.dayOfYear),
+                        earliestDate:earliestDate?  earliestDate.toISOString(): null,
+                        latestDate: latestDate?latestDate.toISOString(): null,
                         section: section,
                         beginNode: beginNode,
                         endNode: endNode
                   })
+                
             })
       }
 
-
-      async function fetchDateAnnotations(baseURL, auth){
-          return  axios.get(`${baseURL}/annotations`,{auth, params: {label: 'DATE'}});
+      async function fetchDateAnnotations(){
+            try {
+                  const response = await axios.get(`${baseURL}/annotations`,{auth, params: {label: 'DATE'}});
+                  return response.data;
+            }catch( error ) {
+                  console.log(`no place refs for section ${sectionId} `);
+                  return null
+            }
       }
 
-      async function fetchDateRefs( refId){
-           return axios.get(`${baseURL}/annotations`, {auth, params: {label: 'DATEREF'}});
+      async function fetchDateRefs(){
+            try {
+                  const response = await axios.get(`${baseURL}/annotations`,{auth, params: {label: 'DATEREF'}});
+                  return response.data;
+            }catch( error ) {
+                  console.log(`no place refs for section ${sectionId} `);
+                  return null
+            }
+          
       }
+
+      async function fetchTranslations(){
+            let translationPromises = [];
+
+            dateList.forEach( dateItem => {
+
+                  let translationPromise = new Promise( resolve=>{
+                        let sectionId  = dateItem.section;
+                        fetchTranslation(sectionId)
+                        .then( translation =>{
+                              if(translation.length > 0 ){
+                                    let translationText = parseTranslation( translation, dateItem.beginNode, dateItem.endNode);
+                                    dateItem.translation = translationText;
+                              }
+                              resolve();
+                       
+                        })
+                  });
+                  translationPromises.push(translationPromise);
+            })
+
+            let lookedUp = await Promise.all(translationPromises)
+            console.log( " all translations for dates look up")
+      }
+
+      async function fetchTranslation(sectionId){
+            try{
+                  const sectionURL = `${baseURL}/section/${sectionId}`;
+                  const response = await axios.get( `${sectionURL}/annotations`, {auth, params: {label: 'TRANSLATION'}})
+                  return response.data;
+            } catch ( error){
+                  console.log( 'error fetching translation for section', sectionId)
+                  return null;
+            }
+           
+      }
+
+      function parseTranslation( translation, dateStart, dateEnd){
+            if( translation.length === 0 )
+            return;
+  
+            for (const entry of translation ) {
+                  const text = entry.properties.text;
+                  const beginTextNode = entry.links[0].type==="BEGIN" ? entry.links[0].target:entry.links[1].target;
+                  const endTextNode = entry.links[0].type==="END" ? entry.links[0].target:entry.links[1].target;
+              
+                  if ( parseInt(dateStart) >= parseInt(beginTextNode) && parseInt(dateEnd) <= parseInt(endTextNode) )
+                        return text;
+            }
+        
+      }
+
 
   
 // these three should be in a shared util file but ... however thisway
