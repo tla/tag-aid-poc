c309e0767a727f3f4dfc627a9364e00f9e6b7036
Athena Capsis <acapsis@accademiaDegliIncogniti.net>
2020-04-03 12:36:37 -0700

    show full title in section list

--- src/components/Edition/SectionList.js
+++ src/components/Edition/SectionList.js
@@ -4,10 +4,6 @@ import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import { Link} from 'react-router-dom'
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 const SectionList = ( props ) =>{
@@ -30,8 +26,8 @@ const SectionList = ( props ) =>{
                                                       <ListItem key={s.sectionId} value={s.sectionId} selected={s.sectionId === sectionId} >
                                                                   <div style={{display:'flex', flexDirection:'column'}}>
                                                                         <Link   to={`${s.sectionId}`} >
-                                                                              <Typography variant="h6">
-                                                                                    {s.englishTitle.substring(0,12)}
+                                                                              <Typography variant="body1" style={{wordWrap:'break-word'}}>
+                                                                                    {s.englishTitle}
                                                                               </Typography>
                                                                         </Link>
                                                                   
