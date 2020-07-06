8e1cb696c9975b3bbf94683b1d5f010a1d8ac385
Sara Stanton <sara.mpa13@gmail.com>
2020-07-05 20:30:19 -0500

    Add Armenian title to masthead

--- src/components/Edition/EditionHeader.js
+++ src/components/Edition/EditionHeader.js
@@ -11,9 +11,14 @@ const EditionHeader = ( props)=>{
             
       <Grid container spacing={0} style={{maxHeight:'112px'}}>
                   <Grid item xs={12} className="header" >
+                    <span style={{display: 'flex', justifyContent: 'space-between'}}>
                         <Typography variant="h5" style={{padding:'12px 42px'}}>
                               The Chronicle of Matthew of Edessa
                         </Typography>
+                        <Typography variant="h5" style={{padding:'12px 42px', alignSelf: 'center', fontSize: '1.25rem'}}>
+                              Ժամանակագրութիւն Մատթէոսի Ուռհայեցւոյ
+                        </Typography>
+                    </span>
                   </Grid>
                   <Grid item xs={12}>
                               <Navigation  onSearch={onSearch} />
